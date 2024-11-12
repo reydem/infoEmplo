1a6a3b9e38ea8eb8156bab49defcc43ab1ccc358
reydem <reydem613@gmail.com>
2024-11-12 14:42:29 -0500

    feat(frontend): agrega configuración de Tailwind CSS y PostCSS al proyecto
    Se añadieron las dependencias 'tailwindcss', 'postcss' y 'autoprefixer' como herramientas de estilo.
    Se generaron los archivos de configuración 'tailwind.config.js' y 'postcss.config.js' para habilitar el uso de clases utilitarias de Tailwind CSS.
    
    El archivo 'tailwind.config.js' se configuró para incluir todos los archivos relevantes del proyecto en la opción 'content'.
    El archivo 'postcss.config.js' fue actualizado para usar 'tailwindcss' y 'autoprefixer' como plugins.
    
    Estos cambios permiten estilizar la interfaz del proyecto con Tailwind CSS, proporcionando un sistema de clases utilitarias eficiente.
    
    Refs: #<número-de-issue>

---
+++ frontend/postcss.config.js
@@ -0,0 +1,7 @@
+// /webapps/infoEmplo-venv/infoEmplo/frontend/postcss.config.js
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+// /webapps/infoEmplo-venv/infoEmplo/frontend/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
