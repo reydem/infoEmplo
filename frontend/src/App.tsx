09b1261f064be9f49fd378e2470189beb9d2ba90
reydem <reydem613@gmail.com>
2024-11-12 15:02:23 -0500

    feat(components): agregar grupo de entrada con ícono de búsqueda al componente App
    Se añadió un InputGroup y el ícono MagnifyingGlassIcon al componente App para mejorar la funcionalidad de búsqueda.
    El InputGroup incluye un ícono y un campo de entrada estilizado, alineados con el diseño de la aplicación.
    
    Además, se creó un archivo index.ts en el directorio components/ui para exportar todos los componentes reutilizables de manera centralizada.
    Esto mejora la organización del proyecto y facilita la importación de componentes.
    
    Se actualizaron las dependencias del proyecto para incluir:
    - @headlessui/react y @heroicons/react para íconos y componentes accesibles.
    - clsx para gestionar clases condicionales.
    - framer-motion para animaciones.
    
    Este cambio mejora la experiencia de usuario y establece una base para futuros componentes dinámicos y estilizados.
    
    Refs: N/A

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { Input, InputGroup } from './components/ui'
+import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
 import './App.css'
 
 function App() {
@@ -8,6 +10,10 @@ function App() {
       <h1 className="text-3xl font-bold underline text-cyan-500">
       Hello world!
     </h1>
+    <InputGroup>
+      <MagnifyingGlassIcon />
+      <Input name="search" placeholder="Search&hellip;" aria-label="Search" />
+    </InputGroup>
     </>
   )
 }
