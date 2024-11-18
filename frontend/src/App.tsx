f64362f9ffedf691040cf3906deda1859e08967b
reydem <reydem613@gmail.com>
2024-11-18 07:06:25 -0500

    feat(frontend): actualización de estructura y recursos del frontend
    Se añadió un nuevo archivo de recursos llamado 'Logo.png' en la carpeta 'assets'.
    La organización de las carpetas y componentes en 'src/components' fue mejorada para separar mejor los componentes principales y los específicos de la interfaz.
    
    Cambios realizados:
    - Se añadió 'Logo.png' a 'src/assets'.
    - Se reestructuró la carpeta 'components' para mover 'Home.tsx' bajo 'Home'.
    - Se mantuvo la consistencia de los nombres en componentes bajo la carpeta 'ui'.
    
    Esta mejora busca facilitar el mantenimiento y escalabilidad del proyecto.
    
    Refs: #2031

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,14 @@
-import { Input, InputGroup } from './components/ui'
-import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
+
 import './App.css'
+import Home from './components/Home/Home'
 
 function App() {
 
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-cyan-500">
-      Hello world!
-    </h1>
-    <InputGroup>
-      <MagnifyingGlassIcon />
-      <Input name="search" placeholder="Search&hellip;" aria-label="Search" />
-    </InputGroup>
+      <Home/>
+  
     </>
   )
 }
