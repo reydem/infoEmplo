baad483d09aac2d8573d967af42facc5cafbc9c7
reydem <reydem613@gmail.com>
2024-11-19 04:54:00 -0500

    feat(frontend): mejora en el diseño y funcionalidad del componente Home
    Refactorización del componente Home para mejorar el diseño y agregar nuevos botones. Cambios clave:
    
    - Se añadió una barra lateral de navegación fija con botones adicionales para distintas funcionalidades.
    - Se introdujeron varias instancias de EmploymentOffer con descripciones de ejemplo.
    - Se agregó una nueva serie de botones con BuildingOffice2Icon en la columna derecha para empresas contratistas.
    - Se actualizó InputGroup con una barra de búsqueda que incluye un icono de MagnifyingGlass.
    
    Estos cambios mejoran la interfaz al crear un diseño más estructurado y agregar más elementos interactivos, optimizando la experiencia del usuario.
    
    Refs: #123 (Mejora de la interfaz en el componente Home)
    BREAKING CHANGE: Modificación en el diseño y estructura del componente Home. Asegúrese de alinear otros componentes utilizando los estilos y activos actualizados.

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,10 +1,9 @@
 // /webapps/infoEmplo-venv/infoEmplo/frontend/src/components/Home/Home.tsx
 import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
-import { SignalIcon, FolderIcon, DocumentCheckIcon, ChartBarIcon } from '@heroicons/react/24/outline';
+import { SignalIcon, FolderIcon, DocumentCheckIcon, ChartBarIcon, BuildingOffice2Icon } from '@heroicons/react/24/outline';
 import Logo from '../../assets/Logo.png';
 import { Button, Input, InputGroup } from '../ui';
 import EmploymentOffer from '../custom-ui/EmploymentOffer';
-
 
 function Home() {
   return (
@@ -95,7 +94,86 @@ function Home() {
           {/* Main area */}
 
         </main>
-        <aside className="sticky top-8 hidden w-44 shrink-0 xl:block border-dashed border-2 border-sky-500">{/* Right column area */}</aside>
+        <aside className="sticky top-8 hidden w-44 shrink-0 xl:block border-dashed border-2 border-sky-500">
+
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between ">
+            <Button color="white" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
+              <BuildingOffice2Icon aria-hidden="true" className="size-6" />
+              Empresa contratista
+            </Button>
+          </div>
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between ">
+            <Button color="white" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
+              <BuildingOffice2Icon aria-hidden="true" className="size-6" />
+              Empresa contratista
+            </Button>
+          </div>
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between ">
+            <Button color="white" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
+              <BuildingOffice2Icon aria-hidden="true" className="size-6" />
+              Empresa contratista
+            </Button>
+          </div>
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between ">
+            <Button color="white" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
+              <BuildingOffice2Icon aria-hidden="true" className="size-6" />
+              Empresa contratista
+            </Button>
+          </div>
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between ">
+            <Button color="white" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
+              <BuildingOffice2Icon aria-hidden="true" className="size-6" />
+              Empresa contratista
+            </Button>
+          </div>
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between ">
+            <Button color="white" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
+              <BuildingOffice2Icon aria-hidden="true" className="size-6" />
+              Empresa contratista
+            </Button>
+          </div>
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between ">
+            <Button color="white" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
+              <BuildingOffice2Icon aria-hidden="true" className="size-6" />
+              Empresa contratista
+            </Button>
+          </div>
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between ">
+            <Button color="white" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
+              <BuildingOffice2Icon aria-hidden="true" className="size-6" />
+              Empresa contratista
+            </Button>
+          </div>
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between ">
+            <Button color="white" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
+              <BuildingOffice2Icon aria-hidden="true" className="size-6" />
+              Empresa contratista
+            </Button>
+          </div>
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between ">
+            <Button color="white" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
+              <BuildingOffice2Icon aria-hidden="true" className="size-6" />
+              Empresa contratista
+            </Button>
+          </div>
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between ">
+            <Button color="white" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
+              <BuildingOffice2Icon aria-hidden="true" className="size-6" />
+              Empresa contratista
+            </Button>
+          </div>
+          <div className="mx-auto flex h-16 max-w-7xl items-center justify-between ">
+            <Button color="white" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-300">
+              <BuildingOffice2Icon aria-hidden="true" className="size-6" />
+              Empresa contratista
+            </Button>
+          </div>
+
+          
+
+          {/* Right column area */}
+
+        </aside>
       </div>
     </div>
   )
