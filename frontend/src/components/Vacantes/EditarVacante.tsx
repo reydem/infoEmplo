2ca8b8301044bba43d239a89c1cf06dfb3de9aa8
reydem <reydem613@gmail.com>
2024-11-29 10:59:48 -0500

    feat(EditarVacante): agregar lógica para consultar producto a editar
    Se implementaron los siguientes cambios:
    - Se añadió el hook 'useParams' para obtener el ID del producto desde la URL.
    - Se creó la función 'consultarAPI' para realizar una solicitud GET al endpoint '/productos/:id' y obtener los datos del producto a editar.
    - Se agregó un estado inicial para almacenar los datos del producto ('nombre', 'precio', 'imagen') mediante el hook 'useState'.
    - Se integró 'useEffect' para realizar la consulta a la API al cargar el componente.
    
    Estos cambios permiten que el componente cargue y muestre los datos del producto correspondiente antes de proceder a su edición.
    
    Refs: N/A

--- frontend/src/components/Vacantes/EditarVacante.tsx
+++ frontend/src/components/Vacantes/EditarVacante.tsx
@@ -1,11 +1,32 @@
 // /webapps/infoEmplo-venv/infoEmplo/frontend/src/components/Vacantes/EditarVacante.tsx
+import { useState, useEffect } from 'react';
+import clienteAxios from '../../config/axios';
+import { useParams } from 'react-router-dom';
 
 function EditarVacante() {
-  return (
-    <div>
-      <h2>Editar Vacante</h2>
-    </div>
-  )
+  // obtener el ID
+  const { id } = useParams();
+  // producto = state, y funcion para actualizar
+  const [producto, guardarProducto] = useState({
+    nombre: '',
+    precio: '',
+    imagen: ''
+  });
+  // consultar la api para traer el producto a editar
+  const consultarAPI = async () => {
+    try {
+      const productoConsulta = await clienteAxios.get(`/productos/${id}`);
+      console.log(productoConsulta);
+      guardarProducto(productoConsulta.data);
+    } catch (error) {
+      console.error('Error al consultar el producto:', error);
+    }
+  };
+  // cuando el componente carga
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+  return (<h2>EditarProducto</h2>);
 }
 
 export default EditarVacante
