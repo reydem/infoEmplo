29c1f51cdba0113f7c3478d844b12535fe7cab88
reydem <reydem613@gmail.com>
2024-11-29 10:33:46 -0500

    feat(NuevoVacante): implementación de formulario para agregar productos
    Se creó un formulario interactivo en el componente NuevoVacante para agregar nuevos productos con los campos necesarios.
    
    Detalles técnicos:
    - Se agregó un estado local para manejar los datos del producto (nombre y precio) y el archivo de imagen.
    - El formulario incluye campos para nombre, precio e imagen, con validaciones mínimas para el precio.
    - Se mantiene la estructura base del componente, con mejoras en la funcionalidad y la presentación para futuros envíos de datos.
    
    Este cambio sienta las bases para la integración de la funcionalidad de creación de productos mediante el frontend.

--- frontend/src/components/Vacantes/NuevoVacante.tsx
+++ frontend/src/components/Vacantes/NuevoVacante.tsx
@@ -1,11 +1,36 @@
 // /webapps/infoEmplo-venv/infoEmplo/frontend/src/components/Vacantes/NuevoVacante.tsx
+import { useState, Fragment } from 'react';
 
 function NuevoVacante() {
+  //producto = state, guardarProducto = setstate
+  const [producto, guardarProducto] = useState({
+    nombre: '',
+    precio: ''
+  });
+  // archivo = state, guardarArchivo = setState
+  const [archivo, guardarArchivo] = useState('');
   return (
-    <div>
-      <h2>Nuevo Vacante</h2>
-    </div>
-  )
+    <Fragment>
+      <h2>Nuevo Producto</h2>
+      <form>
+        <legend>Llena todos los campos</legend>
+        <div className="campo">
+          <label>Nombre:</label>
+          <input type="text" placeholder="Nombre Producto" name="nombre" />
+        </div>
+        <div className="campo">
+          <label>Precio:</label>
+          <input type="number" name="precio" min="0.00" step="0.01" placeholder="Precio" />
+        </div>
+        <div className="campo">
+          <label>Imagen:</label>
+          <input type="file" name="imagen" />
+        </div>
+        <div className="enviar">
+          <input type="submit" className="btn btn-azul" value="Agregar Producto" />
+        </div>
+      </form>
+    </Fragment>
+  );
 }
-
 export default NuevoVacante
