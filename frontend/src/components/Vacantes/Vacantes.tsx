8469228f5445e47d1684165091c18b20fba9d012
reydem <reydem613@gmail.com>
2025-03-17 16:30:12 -0500

    refactor(vacantes): actualizar renderizado y limpiar código
    Se eliminó el código obsoleto relacionado a la navegación y a la simulación de órdenes para centrar el componente en la visualización de vacantes.
    
    Se actualizó el encabezado para mostrar 'Vacantes Disponibles' de forma accesible y se modificó el mapeo para renderizar cada vacante con su título, imagen, descripción y salario ofrecido.
    
    Estos cambios mejoran la legibilidad y coherencia del código en el componente Vacantes.

--- frontend/src/components/Vacantes/Vacantes.tsx
+++ frontend/src/components/Vacantes/Vacantes.tsx
@@ -10,80 +10,7 @@ import { CRMContext } from '../../context/CRMContext';
 
 import { CheckCircleIcon } from '@heroicons/react/20/solid'
 
-const navigation = {
-    categories: [
-        {
-            name: 'Women',
-            clothing: [
-                [
-                    { name: 'Tops', href: '#' },
-                    { name: 'Dresses', href: '#' },
-                    { name: 'Pants', href: '#' },
-                    { name: 'Denim', href: '#' },
-                    { name: 'Sweaters', href: '#' },
-                    { name: 'T-Shirts', href: '#' },
-                ],
-                [
-                    { name: 'Jackets', href: '#' },
-                    { name: 'Activewear', href: '#' },
-                    { name: 'Shorts', href: '#' },
-                    { name: 'Swimwear', href: '#' },
-                    { name: 'Browse All', href: '#' },
-                ],
-            ],
-            accessories: [
-                { name: 'Shoes', href: '#' },
-                { name: 'Jewelry', href: '#' },
-                { name: 'Handbags', href: '#' },
-                { name: 'Socks', href: '#' },
-                { name: 'Hats', href: '#' },
-                { name: 'Browse All', href: '#' },
-            ],
-            categories: [
-                { name: 'New Arrivals', href: '#' },
-                { name: 'Sale', href: '#' },
-                { name: 'Basic Tees', href: '#' },
-                { name: 'Artwork Tees', href: '#' },
-            ],
-        },
-        {
-            name: 'Men',
-            clothing: [
-                [
-                    { name: 'Dress Shirts', href: '#' },
-                    { name: 'Pants', href: '#' },
-                    { name: 'Jackets', href: '#' },
-                    { name: 'T-Shirts', href: '#' },
-                    { name: 'Jeans', href: '#' },
-                    { name: 'Hoodies', href: '#' },
-                ],
-                [
-                    { name: 'Vests', href: '#' },
-                    { name: 'Kilts', href: '#' },
-                    { name: 'Outdoors', href: '#' },
-                    { name: 'Capes', href: '#' },
-                    { name: 'Browse All', href: '#' },
-                ],
-            ],
-            accessories: [
-                { name: 'Watches', href: '#' },
-                { name: 'Boots', href: '#' },
-                { name: 'Fanny Packs', href: '#' },
-                { name: 'Sunglasses', href: '#' },
-                { name: 'Browse All', href: '#' },
-            ],
-            categories: [
-                { name: 'Just Added', href: '#' },
-                { name: 'Clearance', href: '#' },
-                { name: 'Graphic Tees', href: '#' },
-            ],
-        },
-    ],
-    other: [
-        { name: 'Company', href: '#' },
-        { name: 'Stores', href: '#' },
-    ],
-}
+
 const orders = [
     {
         number: 'WU88191111',
@@ -190,75 +117,77 @@ function Vacantes() {
 
     if (!vacantes.length) return <Spinner />;
 
-    
+
 
     return (
         <div className="bg-gray-50 listado-empleados border-gray-400 border-t-4 my-0 ">
             <main className="">
                 <section aria-labelledby="recent-heading" className="">
-                    <h2 id="recent-heading" className="sr-only">
-                        Recent orders
-                    </h2>
+                    <h2 id="recent-heading" className="sr-only">Vacantes Disponibles</h2>
                     <div className="mx-auto max-w-7xl sm:px-2 lg:px-8">
                         <div className="mx-auto max-w-2xl space-y-8 sm:px-4 lg:max-w-4xl lg:px-0">
-                            {orders.map((order) => (
-                                <div
-                                    key={order.number}
-                                    className=""
-                                >
-                                    <h3 className="sr-only">
-                                        Order placed on <time dateTime={order.createdDatetime}>{order.createdDate}</time>
-                                    </h3>
+                            {vacantes.map((vacante) => (
+                                <div key={vacante._id} className="">
+                                    {/* Encabezado con el nombre de la empresa */}
                                     <div className="flex items-center border-b border-gray-200 p-4 sm:grid sm:grid-cols-4 sm:gap-x-6 sm:p-6">
-                                        <dl className="grid flex-1 ">
+                                        <dl className="grid flex-1">
                                             <div>
-                                                <dt className="font-medium text-gray-900 text-2xl ">Total amount</dt>
-
+                                                <dt className="font-medium text-gray-900 text-2xl">
+                                                    {vacante.titulo}
+                                                </dt>
                                             </div>
                                         </dl>
                                     </div>
-                                    {/* Products */}
-                                    <h4 className="sr-only">Items</h4>
+                                    {/* Detalle de la vacante */}
                                     <ul role="list" className="divide-y divide-gray-200">
-                                        {order.products.map((product) => (
-                                            <li key={product.id} className="p-4 sm:p-6">
-                                                <div className="flex items-center sm:items-start">
-                                                    <div className="size-20 shrink-0 overflow-hidden rounded-lg bg-gray-200 sm:size-40">
-                                                        <img alt={product.imageAlt} src={product.imageSrc} className="size-full object-cover" />
-                                                    </div>
-                                                    <div className="ml-6 flex-1 text-sm">
-                                                        <div className="font-medium text-gray-900 sm:flex sm:justify-between">
-                                                            <h5>{product.name}</h5>
-                                                            <p className="mt-2 sm:mt-0">{product.price}</p>
-                                                        </div>
-                                                        <p className="hidden text-gray-500 sm:mt-2 sm:block">{product.description}</p>
-                                                    </div>
+                                        <li className="p-4 sm:p-6">
+                                            <div className="flex items-center sm:items-start">
+                                                <div className="size-20 shrink-0 overflow-hidden rounded-lg bg-gray-200 sm:size-40">
+                                                    <img
+                                                        alt={vacante.titulo}
+                                                        src={
+                                                            vacante.imagen_empresa
+                                                                ? `http://localhost:5000/uploads/${vacante.imagen_empresa}`
+                                                                : "https://via.placeholder.com/150"
+                                                        }
+                                                        className="size-full object-cover"
+                                                    />
                                                 </div>
-                                                <div className="mt-6 sm:flex sm:justify-between">
-                                                    <div className="flex items-center">
-                                                        <CheckCircleIcon aria-hidden="true" className="size-5 text-green-500" />
-                                                        <p className="ml-2 text-sm font-medium text-gray-500">
-                                                            Delivered on <time dateTime={order.deliveredDatetime}>{order.deliveredDate}</time>
-                                                        </p>
+                                                <div className="ml-6 flex-1 text-sm">
+                                                    <div className="font-medium text-gray-900 sm:flex sm:justify-between">
+                                                        <h5>{vacante.titulo}</h5>
+                                                        <p className="mt-2 sm:mt-0">${vacante.salario_ofrecido}</p>
                                                     </div>
-                                                    <div className="hidden lg:col-span-2 lg:flex lg:items-center lg:justify-end lg:space-x-4">
-                                                        <a
-                                                            href={order.invoiceHref}
-                                                            className="flex items-center justify-center rounded-md border border-gray-300 bg-white px-2.5 py-2 text-sm font-medium text-gray-700 shadow-xs hover:bg-gray-50 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden"
-                                                        >
-                                                            <span>View Invoice</span>
-                                                            <span className="sr-only">for order {order.number}</span>
-                                                        </a>
-                                                    </div>
+                                                    <p className="hidden text-gray-500 sm:mt-2 sm:block">
+                                                        {vacante.descripcion}
+                                                    </p>
                                                 </div>
-                                            </li>
-                                        ))}
+                                            </div>
+                                        </li>
                                     </ul>
+                                    <div className="mt-6 sm:flex sm:justify-between">
+                                        <div className="flex items-center">
+                                            <CheckCircleIcon aria-hidden="true" className="size-5 text-green-500" />
+                                            <p className="ml-2 text-sm font-medium text-gray-500">
+                                                Delivered on <time ></time>
+                                            </p>
+                                        </div>
+                                        <div className="hidden lg:col-span-2 lg:flex lg:items-center lg:justify-end lg:space-x-4">
+                                            <a
+                                                href="#"
+                                                className="flex items-center justify-center rounded-md border border-gray-300 bg-white px-2.5 py-2 text-sm font-medium text-gray-700 shadow-xs hover:bg-gray-50 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden"
+                                            >
+                                                <span>View Invoice</span>
+                                                <span className="sr-only">for order </span>
+                                            </a>
+                                        </div>
+                                    </div>
                                 </div>
                             ))}
                         </div>
                     </div>
                 </section>
+
             </main>
         </div>
     )
@@ -271,24 +200,26 @@ export default Vacantes;
 
 
 
-    // return (
-    //     <Fragment>
-    //         <h2>Vacantes</h2>
-    //         <Link to={'/vacantes/nuevo'} className="btn btn-verde nvo-cliente">
-    //             <i className="fas fa-plus-circle"></i>
-    //             Nueva Vacante
-    //         </Link>
-    //         <ul className="listado-vacantes">
-    //             {vacantes.map((vacante) => (
-    //                 <Vacante
-    //                     key={vacante._id}
-    //                     vacante={vacante}
-    //                     setActualizarVacantes={setActualizarVacantes}
-    //                 />
-    //             ))}
-    //         </ul>
-    //     </Fragment>
-    // );
+// return (
+//     <Fragment>
+//         <h2>Vacantes</h2>
+//         <Link to={'/vacantes/nuevo'} className="btn btn-verde nvo-cliente">
+//             <i className="fas fa-plus-circle"></i>
+//             Nueva Vacante
+//         </Link>
+//         <ul className="listado-vacantes">
+//             {vacantes.map((vacante) => (
+//                 <Vacante
+//                     key={vacante._id}
+//                     vacante={vacante}
+//                     setActualizarVacantes={setActualizarVacantes}
+//                 />
+//             ))}
+//         </ul>
+//     </Fragment>
+// );
+
+
 
 
 
